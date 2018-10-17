937d5d19e65a312d3d9b88aeeb7b71a7861617d7
aaaaus <ausluft@gmail.com>
2018-10-17 13:54:09 -0400

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 1, 'male', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 3, 'male', 'brown', 'aggressive', 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 7, 'female', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 2, 'male', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 15, 'female', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 11, 'male', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 4, 'male', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 9, 'male', 'brown', 'aggressive', 1);
